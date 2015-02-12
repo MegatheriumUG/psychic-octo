0a57c47d6e44d62cd3ff0357630c335b779d26b6
SargTeX <martin.christian.bories@gmail.com>
2015-02-12 20:51:47 +0100

    API documentation and some controllers
    added some api documentation in german
    added some project controllers: ProjectList and ProjectAdd

--- src/controller/Project.js
+++ src/controller/Project.js
@@ -2,7 +2,28 @@ var db = require('mongoose'),
 	Project = require('./model/Project.js');
 
 exports.setup = function(app) {
-	app.get('/ProjectList', function(req, res) {
+	app.get('/ProjectList', function(req, res, jump) {
+		var options = {};
+		if (req.param('companyId')) options.companies = req.param('companyId');
 
+		Project.find(options)
+			.exec(function(err, projects) {
+				if (err) return jump(err);
+
+				res.send({template: 'ProjectList', data: {projects: projects}});
+			});
+	});
+
+	app.post('/ProjectAdd', function(req, res, jump) {
+		var project = new Project({
+			name: req.param('name'),
+			companies: typeof req.param('companies') === 'string' ? [req.param('companies')] : req.param('companies')
+		});
+
+		if (!project.companies || project.companies.length == 0) return res.send({status: 'error', errors: ['Du dem Projekt ein Unternehmen zuordnen.']});
+		project.save(function(err) {
+			if (err) return jump(err);
+			res.send({status: 'success', data: {projectId: project._id}});
+		});
 	});
 };
