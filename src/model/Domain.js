950d83cc80d4e1b7e2a98be6c220351405282f58
SargTeX <martin.christian.bories@gmail.com>
2015-02-19 13:20:22 +0100

    Setup fixed, added domain target
    You can now set the ip each domain is targeting

--- src/model/Domain.js
+++ src/model/Domain.js
@@ -3,7 +3,8 @@ var ObjectId = db.Schema.Types.ObjectId;
 
 var Domain = new db.Schema({
 	domain: {type: String, required: true, unique: true},
-	company: {type: ObjectId, ref: 'Company'}
+	company: {type: ObjectId, ref: 'Company'},
+	target: {type: String} // target ip address
 });
 
 module.exports = db.model('Domain', Domain);
